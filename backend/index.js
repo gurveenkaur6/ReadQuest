2b829f0ac6c62b2f827805535f1e2644ea3c96e5
Gurveen <gurveenkaursahni@gmail.com>
2024-08-13 21:46:42 -0400

    created a route handler

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ import {PORT} from "./config.js";
 //  an instance of the Express application
 const app = express();
 
-// a route handler for the root URL (/), when a client makes a GET request to the route URL, the callback function is executed
+// route handler for the root URL (/), when a client makes a GET request to the route URL, the callback function is executed
 app.get('/',(request, response)=>{
     console.log(request); //  the incoming HTTP request which contains information such as headers, query parameters, and request body.
     return response.status(664).send("ReadQuest is Work in Progress. Stay Tuned :)"); // the outging response sent to the client 
