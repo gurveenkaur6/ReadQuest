c3d03463f4555c8b005f5e8010a131191aa4db19
Gurveen <gurveenkaursahni@gmail.com>
2024-08-16 16:32:08 -0400

    added react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+// import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import ShowBook from './pages/ShowBook';
+import EditBook from './pages/EditBook';
+import DeleteBook from './pages/DeleteBook';
+import CreateBooks from './pages/CreateBooks';
+
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>Welcome to my App</div>
+    <Routes>
+      <Route path= '/' element={<Home />} />
+      <Route path= '/books/create' element={<CreateBooks />} />
+      <Route path= '/books/details/:id' element={<ShowBook/>} />
+      <Route path= '/books/edit/:id' element={<EditBook/>} />
+      <Route path= '/books/delete/:id' element={<DeleteBook/>} />
+    </Routes>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+// BrowserRouter enables your app to have multiple pages to navigate between different components or views without reloading the entire page.
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>
 )
