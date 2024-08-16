74adca28e647626dafd2c2ce40bc41093a6fba21
Gurveen <gurveenkaursahni@gmail.com>
2024-08-16 15:34:58 -0400

    Enabled CORS policy for all routes

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,22 @@ import {PORT, mongoDBURL} from "./config.js";
 import mongoose from "mongoose";
 import Book from "./models/bookModel.js";
 import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 //  an instance of the Express application
 const app = express();
 
-// we need a middleware to parse the request body
+// Enable CORS policy on express backend to allow frontend to make requests to it
+// Enable CORS for all routes
+app.use(cors()); // Option 1 :cors(*) to allow all origins
+// Option 2 :Only allow custom origins
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'PUT', 'POST', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+}))
+
+// a middleware to parse the request body- middleware function in express framework sits between client request and server response
 app.use(express.json());
 
 app.use('/books', booksRoute);
