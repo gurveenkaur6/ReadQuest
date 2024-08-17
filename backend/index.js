b45ff28155a1dae881c64c1c37e889ddb5a79c5f
Gurveen <gurveenkaursahni@gmail.com>
2024-08-16 21:40:17 -0400

    edited home.jsx to show books in a list

--- backend/index.js
+++ backend/index.js
@@ -10,13 +10,13 @@ const app = express();
 
 // Enable CORS policy on express backend to allow frontend to make requests to it
 // Enable CORS for all routes
-// app.use(cors()); // Option 1 :cors(*) to allow all origins
+app.use(cors()); // Option 1 :cors(*) to allow all origins
 // Option 2 :Only allow custom origins
-app.use(cors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'PUT', 'POST', 'DELETE'],
-    allowedHeaders: ['Content-Type'],
-}))
+// app.use(cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'PUT', 'POST', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+// }))
 
 // a global middleware to parse the request body- middleware function in express framework sits between client request and server response
 app.use(express.json()); // converts JSON data in the request body to a javascript object 
