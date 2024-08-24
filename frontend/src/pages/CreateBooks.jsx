b033defe9eabae6242481c39fccd0f175f3c5ab4
Gurveen <gurveenkaursahni@gmail.com>
2024-08-24 16:01:40 -0400

    modified CreateBooks.jsx

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -6,36 +6,34 @@ import BackButton from "../components/BackButton";
 import Spinner from "../components/Spinner";
 
 const CreateBooks = () => {
-
   // define state variabes - they hold book's title, author, publishYear and loading status
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
-  const [pubishYear, setPublishYear] = useState('');
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [pubishYear, setPublishYear] = useState("");
   const [loading, setLoading] = useState(false);
 
   // sets up navigation to allow component CreateBooks to change pages
   const navigate = useNavigate();
 
   // function to save the newly created book
-  const handleSaveBook = ()=>{
-
+  const handleSaveBook = () => {
     // create an object with the book's information
     const data = {
-      title, 
-      author, 
-      publishYear
+      title,
+      author,
+      publishYear,
     };
     setLoading(true);
     // using axios to send the book data to the server at port 5555/books
     axios
       .post("http://localhost:5555/books")
       // if successfully sent, navigate back to home page
-      .then(()=>{
+      .then(() => {
         setLoading(false);
-        navigate('/'); 
+        navigate("/");
       })
       // else, alert the user of the error and log the error in the console
-      .catch((error)=>{
+      .catch((error) => {
         setLoading(false);
         alert("An error hhappened ! Please check console");
         console.log(error);
@@ -43,8 +41,48 @@ const CreateBooks = () => {
   };
   // this is what the component displays
   return (
-    <div>CreateBooks</div>
-  )
-}
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3xl my-4">Create Book</h1>
+      {loading ? <Spinner /> : ""}
+      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
 
-export default CreateBooks
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Title</label>
+          <input 
+            type = "text" 
+            value = {title} 
+            // OnChange is triggered whenever the input changes
+            onChange={(e)=> setTitle(e.target.value)} // e.target.value is accessing the current value of the input field
+            className="border-2 border-gray-500 px-4 py-2 w-full" 
+          />
+        </div>
+
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Author</label>
+          <input 
+            type = "text" 
+            value = {author} 
+            // OnChange is triggered whenever the input changes
+            onChange={(e)=> setAuthor(e.target.value)} // e.target.value is accessing the current value of the input field
+            className="border-2 border-gray-500 px-4 py-2 w-full" 
+          />
+        </div>
+
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Publish Year</label>
+          <input 
+            type = "text" 
+            value = {pubishYear} 
+            // OnChange is triggered whenever the input changes
+            onChange={(e)=> setPublishYear(e.target.value)} // e.target.value is accessing the current value of the input field
+            className="border-2 border-gray-500 px-4 py-2 w-full" 
+          />
+        </div>
+        <button className="p-2 bg-sky-500 m-8" onClick={handleSaveBook}>Save</button>
+      </div>
+    </div>
+  );
+};
+
+export default CreateBooks;
