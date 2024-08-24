db3ad5ba29c8f236052ca82fed9f0345afd4b1c5
Gurveen <gurveenkaursahni@gmail.com>
2024-08-24 16:11:14 -0400

    modified EditBooks.jsx

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -1,6 +1,19 @@
 // import React from 'react'
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import BackButton from "../components/BackButton";
+import Spinner from "../components/Spinner";
 
+// Similar to create book but we fetch the book to be modified using its id from the backend
 const EditBook = () => {
+
+  // define the state variables
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [publishYear, setPublishYear] = useState('');
+  const [loading, setLoading] = useState(false);
+  
   return (
     <div>EditBook</div>
   )
