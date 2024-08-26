a68ec48950a699de21b68ea1510de5b8f49088e5
Gurveen <gurveenkaursahni@gmail.com>
2024-08-26 16:31:15 -0400

    refractored BooksCard.jsx

--- frontend/src/components/home/BooksTable.jsx
+++ frontend/src/components/home/BooksTable.jsx
@@ -4,7 +4,7 @@ import { AiOutlineEdit } from 'react-icons/ai';
 import { BsInfoCircle } from 'react-icons/bs';
 import { MdOutlineDelete } from 'react-icons/md';
 
-// eslint-disable-next-line react/prop-types
+ 
 const BooksTable = ({ books }) => {
   return (
     <table className='w-full border-separate border-spacing-2'>
