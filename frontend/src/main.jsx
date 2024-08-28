fd345b5e6fa25d62beff768aaf00e2ae10c80c96
Gurveen <gurveenkaursahni@gmail.com>
2024-08-28 16:31:46 -0400

    added SnackbarProvider to the app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,15 @@
 // import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 // BrowserRouter enables your app to have multiple pages to navigate between different components or views without reloading the entire page.
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from "react-router-dom";
+import { SnackbarProvider } from "notistack";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <SnackbarProvider>
+      <App />
+    </SnackbarProvider>
   </BrowserRouter>
-)
+);
